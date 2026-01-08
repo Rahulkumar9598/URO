8d90362e0a5442671d817d0e9399daaa7c186db2
Rahul <palrahul95987@gmail.com>
2026-01-08 11:28:16 +0530

    fix: correct casing for userSlice import in login.jsx

--- frontend/src/pages/BoysFashion.jsx
+++ frontend/src/pages/BoysFashion.jsx
@@ -6,9 +6,9 @@ import { useState } from 'react';
 import axios from 'axios'
 import api from '../services/endpoint.js';
 import { useDispatch, useSelector } from 'react-redux';
-import { addToCart, increatement, decrement } from '../Store/CartSlice.js';
+import { addToCart, increatement, decrement } from '../Store/cartSlice.js';
 
-import { toggleLike } from '../Store/WishlistSlice.js';
+import { toggleLike } from '../Store/wishlistSlice.js';
 
 
 const BoysFashion = () => {
